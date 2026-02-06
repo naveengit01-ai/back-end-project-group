e40ddf0a0153d68c3575ff464cd0426e2a4ccd2e
Naveen <kolanaveen797@gmail.com>
2026-02-06 10:52:15 +0530

    addeded

--- Mongod.js
+++ Mongod.js
@@ -17,7 +17,17 @@ const app = express();
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://back-end-project-group.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+
 
 /* ================= DATABASE ================= */
 mongoose
